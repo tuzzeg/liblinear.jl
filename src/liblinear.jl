003e169f4141ab027eadf97ab485fa2651a578e9
Dmitry <deemonster@gmail.com>
2014-06-14 03:56:09 -0700

    Add test methods for library train method.

--- src/liblinear.jl
+++ src/liblinear.jl
@@ -11,4 +11,9 @@ end
 
 Params(; solver::Int=0, C::Float64=1.0, eps::Float64=0.1) = Params(solver, C, eps)
 
+# Cases:
+# - Classification/Regression
+# - 2 Class/Multiclass
+# - Bias/no bias
+
 end # module
