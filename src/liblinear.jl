c6c7db8cfc8c4e9c7878490dbfff3acc7257a0f8
Dmitry <deemonster@gmail.com>
2014-06-13 23:47:32 -0700

    Extract method to create liblinear model from dense input.

--- src/liblinear.jl
+++ src/liblinear.jl
@@ -3,4 +3,12 @@ module liblinear
 include("c.jl")
 import .c: FeatureNode, Parameter, Problem
 
+immutable Params
+  solver::Int
+  C::Float64
+  eps::Float64
+end
+
+Params(; solver::Int=0, C::Float64=1.0, eps::Float64=0.1) = Params(solver, C, eps)
+
 end # module
