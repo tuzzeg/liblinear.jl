fca3ddaefb72496addfa2204e1db290d7e0f267c
Dmitry <deemonster@gmail.com>
2014-06-17 23:27:19 -0700

    Refactor: introduce separate params/model for classification and regression.

--- src/liblinear.jl
+++ src/liblinear.jl
@@ -1,35 +1,79 @@
 module liblinear
 
+import StatsBase: StatisticalModel, fit
+
 include("c.jl")
 import .c: FeatureNode, Parameter, Problem, valid_solver
 
-immutable Params
+immutable ClassificationParams{N, Y}
   solver::Int
   C::Float64
   eps::Float64
-  function Params(solver::Int, C::Float64, eps::Float64)
-    if eps <= 0
-      throw(ArgumentError("eps <= 0"))
-    end
-    if C <= 0
-      throw(ArgumentError("C <= 0"))
-    end
-    if !valid_solver(solver)
-      throw(ArgumentError("Unknown solver, solver=$solver"))
-    end
+  labels::NTuple{N, Y} # expected target values
+  function ClassificationParams(solver::Int, C::Float64, eps::Float64, labels::NTuple{N, Y})
+    _check_params(solver, C, eps)
+    new(solver, C, eps, labels)
+  end
+end
+
+function ClassificationParams{N, Y}(labels::NTuple{N, Y}; solver::Int=0, C::Float64=1.0, eps::Float64=0.1)
+  ClassificationParams{N, Y}(solver, C, eps, labels)
+end
+
+immutable RegressionParams
+  solver::Int
+  C::Float64
+  eps::Float64
+  function RegressionParams(solver::Int, C::Float64, eps::Float64)
+    _check_params(solver, C, eps)
     new(solver, C, eps)
   end
 end
 
-Params(; solver::Int=0, C::Float64=1.0, eps::Float64=0.1) = Params(solver, C, eps)
+function RegressionParams(; solver::Int=0, C::Float64=1.0, eps::Float64=0.1)
+  RegressionParams(solver, C, eps)
+end
 
-immutable Model
-  classes::Array{Int, 1}
-  features::Int
+immutable ClassificationModel{Y} <: StatisticalModel
+  labels::NTuple{2, Y}
   weights::Array{Float64, 2}
 end
 
-function problem{X, Y}(x::Array{X, 2}, y::Array{Y, 1})
+immutable RegressionModel
+  weights::Array{Float64, 1}
+end
+
+# Fit regression model for the given x,y
+# x should be of type convertible to Float64
+function fit{X, Y}(::Type{RegressionModel}, x::Array{X, 2}, y::Array{Y, 1}, params::ClassificationParams{2, Y}; verbose::Bool=false)
+  c.set_silence(!verbose)
+
+  prob_c = _problem(x, y)
+  param_c = _parameter(params)
+  p_model = c.train(prob_c, param_c)
+  model = unsafe_load(p_model)
+
+  # TODO correctly free mem after train
+  n_features = int(model.nr_feature)
+  n_classes = int(model.nr_class)
+  w = pointer_to_array(model.w, n_features)
+  # labels = pointer_to_array(model.label, n_classes)
+  RegressionModel(w)
+end
+
+function _check_params(solver, C, eps)
+  if eps <= 0
+    throw(ArgumentError("eps <= 0, eps=$eps"))
+  end
+  if C <= 0
+    throw(ArgumentError("C <= 0, C=$eps"))
+  end
+  if !valid_solver(solver)
+    throw(ArgumentError("Unknown solver, solver=$solver"))
+  end
+end
+
+function _problem{X, Y}(x::Array{X, 2}, y::Array{Y, 1})
   rows, cols = size(x)
   if rows != length(y)
     throw(ArgumentError("x and y dimentions should match, x=$(size(x)) y=$(size(y))"))
@@ -60,24 +104,12 @@ function problem{X, Y}(x::Array{X, 2}, y::Array{Y, 1})
   Problem(rows, cols, pointer(y_c), pointer(p_nodes), -1)
 end
 
-function parameter(p::Params)
+function _parameter(p::ClassificationParams)
   Parameter(p.solver, p.eps, p.C, 0, C_NULL, C_NULL, 0.1)
 end
 
-function train{X, Y}(params::Params, x::Array{X, 2}, y::Array{Y, 1}; verbose::Bool=false)
-  c.set_silence(!verbose)
-
-  prob_c = problem(x, y)
-  param_c = parameter(params)
-  p_model = c.train(prob_c, param_c)
-  model = unsafe_load(p_model)
-
-  # TODO correctly free mem after train
-  n_features = int(model.nr_feature)
-  n_classes = int(model.nr_class)
-  w = pointer_to_array(model.w, (1, n_features))
-  labels = pointer_to_array(model.label, n_classes)
-  Model(labels, n_features, w)
+function _parameter(p::RegressionParams)
+  Parameter(p.solver, p.eps, p.C, 0, C_NULL, C_NULL, 0.1)
 end
 
 # Cases:
--- test/test_train.jl
+++ test/test_train.jl
@@ -1,24 +1,21 @@
 using Base.Test
 
+import StatsBase: fit
+
 require("src/liblinear.jl")
-import liblinear: Params, Model, train
+import liblinear: ClassificationParams, RegressionModel
+
+x = [ -1  0    1  0;
+      -1  0    0  1;
+       0 -1 -0.5  0;
+       1 -1    0 -1
+    ]
 
 function test_train()
-  #+1 1:-1 3:1
-  #+1 1:-1 4:1
-  #-1 2:-1 3:-0.5
-  #-1 1:1 2:-1 4:-1
-
-  params = Params()
-
-  x = [ -1  0 1 0;
-        -1  0 0 1;
-         0 -1 -0.5 0;
-         1 -1 0 -1
-      ]
+  params = ClassificationParams((-1, 1))
   y = [1, 1, -1, -1]
 
-  model = train(params, x, y)
+  model = fit(RegressionModel, x, y, params)
   dump(model)
 
   @test_approx_eq_eps [-0.67219, 0.49949, 0.41542, 0.41921] model.weights 1e-4
