4f023f67188957fcb0517b7de8e7dc3a5a492115
Dmitry <deemonster@gmail.com>
2014-06-20 04:17:41 -0700

    Add sparse dataset for lib test.

--- test/lib/train.sh
+++ test/lib/train.sh
@@ -2,3 +2,6 @@
 
 train -s 0 iris_1.train.xy iris_1.model
 predict -b 1 iris_1.test.xy iris_1.model iris_1.res.xy
+
+train -s 0 sparse_1.train.xy sparse_1.model
+predict -b 1 sparse_1.test.xy sparse_1.model sparse_1.res.xy
