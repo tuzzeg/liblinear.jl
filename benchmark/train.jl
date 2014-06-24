f0895104ee9d32dff6384e434d607172dd5fbafb
Dmitry <deemonster@gmail.com>
2014-06-24 03:27:54 -0700

    Fix benchmark script - different number of features in train/test.

--- benchmark/train.jl
+++ benchmark/train.jl
@@ -5,7 +5,7 @@ import StatsBase: fit, predict
 import MLBase: roc, recall, precision, f1score
 import liblinear: ClassificationParams, ClassificationModel, RegressionModel
 
-function read(file)
+function read(file; features=0)
   open(ARGS[1], "r") do f
     x, y = read_svmlight(f)
     return x, convert(Array{Int}, y)
@@ -13,12 +13,13 @@ function read(file)
 end
 
 function run(file_prefix, silent=false)
-  train_file = "$file_prefix.train"
-  test_file = "$file_prefix.test"
+  train_file = "$file_prefix"
+  test_file = "$file_prefix.t"
 
   t0 = time()
   x_train, y_train = read(train_file)
-  x_test, y_test = read(test_file)
+  r_train, c_train = size(x_train)
+  x_test, y_test = read(test_file; features=c_train)
   t1 = time()
 
   params = ClassificationParams((-1, 1); eps=0.01)
@@ -30,8 +31,9 @@ function run(file_prefix, silent=false)
 
   if !silent
     rc = roc(y_test, y1)
-    @printf "julia: precision=%.5f recall=%.5f f1=%.5f\n" precision(rc) recall(rc) f1score(rc)
-    @printf "julia: read=%.5f train=%.5f test=%.5f\n" t1-t0 t_train-t1 t_test-t_train
+    print("julia: train=$(size(x_train)) test=$(size(x_test))")
+    @printf "  precision=%.5f recall=%.5f f1=%.5f\n" precision(rc) recall(rc) f1score(rc)
+    @printf "  read=%.5f train=%.5f test=%.5f\n" t1-t0 t_train-t1 t_test-t_train
   end
 end
 
--- benchmark/train.py
+++ benchmark/train.py
@@ -6,12 +6,18 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 prefix_file = sys.argv[1]
-train_file = '%s.train' % prefix_file
-test_file = '%s.test' % prefix_file
+train_file = prefix_file
+test_file = '%s.t' % prefix_file
 
 t0 = time.clock()
 x_train, y_train = load_svmlight_file(train_file)
+r_train, c_train = x_train.shape
+
 x_test, y_test = load_svmlight_file(test_file)
+r_test, c_test = x_test.shape
+
+if c_train != c_test:
+  x_test = x_test[:, 0:c_train]
 t1 = time.clock()
 
 m = LogisticRegression(C = 1)
@@ -25,5 +31,6 @@ recall = recall_score(y_test, y1_test)
 accuracy = accuracy_score(y_test, y1_test)
 f1 = f1_score(y_test, y1_test)
 
-print "python: precision=%.5f recall=%.5f f1=%.5f" % (precision, recall, f1)
-print "python: read=%.5f train=%.5f test=%.5f" % (t1-t0, t_train-t1, t_test-t_train)
+print "python: train=%s test=%s [%s]" % (x_train.shape, x_train.shape, prefix_file)
+print "  precision=%.5f recall=%.5f f1=%.5f" % (precision, recall, f1)
+print "  read=%.5f train=%.5f test=%.5f" % (t1-t0, t_train-t1, t_test-t_train)
