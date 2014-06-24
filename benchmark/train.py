6b15dc04a3ffd6552bd3dadb8e11f911c61d2017
Dmitry <deemonster@gmail.com>
2014-06-23 23:03:23 -0700

    Provide train/test file as parameter in python benchmark.

--- benchmark/train.py
+++ benchmark/train.py
@@ -1,12 +1,17 @@
+import sys
 import time
 
 from sklearn.datasets import load_svmlight_file
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
+prefix_file = sys.argv[1]
+train_file = '%s.train' % prefix_file
+test_file = '%s.test' % prefix_file
+
 t0 = time.clock()
-x_train, y_train = load_svmlight_file("data/australian.train")
-x_test, y_test = load_svmlight_file("data/australian.test")
+x_train, y_train = load_svmlight_file(train_file)
+x_test, y_test = load_svmlight_file(test_file)
 t1 = time.clock()
 
 m = LogisticRegression(C = 1)
@@ -21,4 +26,4 @@ accuracy = accuracy_score(y_test, y1_test)
 f1 = f1_score(y_test, y1_test)
 
 print "python: precision=%.5f recall=%.5f f1=%.5f" % (precision, recall, f1)
-print "python: read=%.3f train=%.3f test=%.3f" % (t1-t0, t_train-t1, t_test-t_train)
+print "python: read=%.5f train=%.5f test=%.5f" % (t1-t0, t_train-t1, t_test-t_train)
